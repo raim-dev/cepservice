56e8a083d5a93e4014cfc5841898206f7bf3eb62
raipereira <raipereira84@gmail.com>
2025-06-15 20:24:55 -0700

    Config git action to trigger Lambda _ API Gateway

---
+++ src/main/java/com/br/cepservice/LambdaHandler.java
@@ -0,0 +1,24 @@
+package com.br.cepservice;
+
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LambdaHandler implements RequestHandler<AwsProxyRequest, AwsProxyResponse> {
+    private static final SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+
+    static {
+        try {
+            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(Application.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Falha ao inicializar Lambda handler", e);
+        }
+    }
+
+    @Override
+    public AwsProxyResponse handleRequest(AwsProxyRequest input, Context context) {
+        return handler.proxy(input, context);
+    }
+}
